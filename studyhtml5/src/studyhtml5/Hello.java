b27dad8513cd1e94543021e2cb34892d0c666f9b
SamuelKimgit <rlatkandpf30@naver.com>
2022-05-16 17:02:09 +0900

    20220516

--- studyhtml5/src/studyhtml5/Hello.java
+++ studyhtml5/src/studyhtml5/Hello.java
@@ -1,5 +1,12 @@
 package studyhtml5;
 
-public class Hello {
+import org.apache.log4j.Logger;
 
+public class Hello {
+	final static Logger LOG = Logger.getLogger(Hello.class); 
+	public static void main(String[] args) {
+		LOG.debug("==================");
+		LOG.debug("=\"Hello\"=");
+		LOG.debug("==================");
+	}
 }
